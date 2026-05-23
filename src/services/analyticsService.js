a42559017c524360e52c2f04d216e9aafae3257a
Mohammed Hasan <mdhasanshuvo220@gmail.com>
2026-05-23 16:53:52 +0600

    expand admin analytics

--- src/services/analyticsService.js
+++ src/services/analyticsService.js
@@ -6,7 +6,7 @@ const { expireDueSubscriptions } = require('./subscriptionService');
 async function getAdminAnalytics() {
   await expireDueSubscriptions({});
 
-  const [totalUsers, totalActiveSubscriptions, revenueAggregation, subscriptionsByPlan, expiredSubscriptions] = await Promise.all([
+  const [totalUsers, totalActiveSubscriptions, revenueAggregation, subscriptionsByPlan, expiredSubscriptions, subscriptionsByStatus, revenueByPlan] = await Promise.all([
     User.countDocuments(),
     Subscription.countDocuments({ status: 'Active' }),
     Subscription.aggregate([
@@ -53,6 +53,59 @@ async function getAdminAnalytics() {
       },
     ]),
     Subscription.countDocuments({ status: 'Expired' }),
+    Subscription.aggregate([
+      {
+        $group: {
+          _id: '$status',
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          status: '$_id',
+          count: 1,
+        },
+      },
+      {
+        $sort: { status: 1 },
+      },
+    ]),
+    Subscription.aggregate([
+      {
+        $match: {
+          paymentStatus: { $in: ['Success', 'Renewed'] },
+        },
+      },
+      {
+        $group: {
+          _id: '$plan',
+          revenue: { $sum: '$priceAtPurchase' },
+        },
+      },
+      {
+        $lookup: {
+          from: 'plans',
+          localField: '_id',
+          foreignField: '_id',
+          as: 'plan',
+        },
+      },
+      {
+        $unwind: '$plan',
+      },
+      {
+        $project: {
+          _id: 0,
+          planId: '$plan._id',
+          planName: '$plan.planName',
+          revenue: 1,
+        },
+      },
+      {
+        $sort: { planName: 1 },
+      },
+    ]),
   ]);
 
   return {
@@ -61,6 +114,8 @@ async function getAdminAnalytics() {
     totalRevenue: revenueAggregation[0]?.totalRevenue || 0,
     subscriptionsByPlan,
     expiredSubscriptions,
+    subscriptionsByStatus,
+    revenueByPlan,
   };
 }
 
